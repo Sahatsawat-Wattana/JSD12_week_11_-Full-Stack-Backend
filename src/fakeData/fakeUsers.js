b8055d5f64e0af9f6f5553badedf7bac1d41a03c
Sahatsawat-Wattana <sahatsawat.wattanapraditchai@gmail.com>
2026-05-13 14:52:00 +0700

    Setup express server

---
+++ src/fakeData/fakeUsers.js
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    id: "1",
+    username: "Marty",
+    email: "marty@example.com",
+    password: "pass123",
+  },
+  {
+    id: "2",
+    username: "Leon",
+    email: "Leon@example.com",
+    password: "pass123",
+  },
+  {
+    id: "3",
+    username: "Tony",
+    email: "Tony@example.com",
+    password: "pass123",
+  },
+];
