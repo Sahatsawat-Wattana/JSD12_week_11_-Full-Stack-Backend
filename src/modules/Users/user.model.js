cf64e57f2489e71afdd9e11e2f2377cf0b0cb584
Sahatsawat-Wattana <sahatsawat.wattanapraditchai@gmail.com>
2026-05-20 14:59:21 +0700

    debug

--- src/modules/Users/user.model.js
+++ src/modules/Users/user.model.js
@@ -11,14 +11,17 @@ const userSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-userSchema.pre("save",async function(next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password,12);
-  next();
-})
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return ;
+  try {
+    this.password = await bcrypt.hash(this.password, 12);
+  } catch (error) {
+    next(error);
+  }
+});
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword,this.password);
-}
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
